dd7984cb0ff260639d3d7c912b23828c9c54b658
Robert Tate <robertjosephtate@gmail.com>
2018-05-22 10:08:38 -0500

    initial server commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const CLIENT_DEST = path.join(__dirname, './client/dist');
+
+module.exports = {
+    entry: ['babel-polyfill', './client/src/index.js'],
+    output: { path: CLIENT_DEST, filename: 'bundle.js' },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'react']
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        publicPath: '/dist/images',
+                        outputPath: 'images'
+                    }
+                }
+            },
+            {
+                test: /\.(pdf)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        publicPath: '/dist/docs',
+                        outputPath: 'docs'
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+}
